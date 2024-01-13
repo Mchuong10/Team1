77800f3867bb3756204795f8e539601bad9a0776
Marie <122072731+Mchuong10@users.noreply.github.com>
2024-01-13 14:28:09 -0800

    Update hw2.js

---
+++ HW2/hw2.js
@@ -0,0 +1,36 @@
+let numberOfWords = 3;
+let wordArray = [];
+
+function transformWords() {
+    for (let i = 0; i < numberOfWords; i++) {
+        let wordEntered = prompt("Please enter a word:");
+        if (wordEntered === null || wordEntered.trim() === "") {
+            alert("Please enter a valid word. Try Again");
+            i--;
+        } else {
+            wordArray.push(wordEntered);
+        }
+    }
+
+    const ul01 = document.getElementById("ul01");
+    ul01.innerHTML = ""; // Clear previous content
+
+    for (let i = 0; i < numberOfWords; i++) {
+        const li = document.createElement("li");
+        li.id = "li" + i;
+        li.innerHTML = wordArray[i];
+        ul01.appendChild(li);
+    }
+
+    const firstSection = document.getElementById('firstSection');
+    const secondSection = document.getElementById("secondSection");
+    const isClicked = !firstSection.classList.contains('hidden');
+
+    if (isClicked) {
+        firstSection.classList.add('hidden');
+        secondSection.classList.remove('hidden');
+    } else {
+        firstSection.classList.remove('hidden');
+        secondSection.classList.add('hidden');
+    }
+}
