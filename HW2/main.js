88c89c47e243b4c863751abad78818cf51fc52f7
Marie <122072731+Mchuong10@users.noreply.github.com>
2024-01-13 21:25:09 -0800

    Update main.js

--- HW2/main.js
+++ HW2/main.js
@@ -1,39 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
 let numberOfWords = 3;
 let wordArray = [];
+let currentSection = 'firstSection';
 
-for (let i = 0; i < numberOfWords; i++) {
-    let wordEntered = prompt("Please enter a word:");
-    if (wordEntered === null || wordEntered.trim() === "") {
-        alert("Please enter a valid word. Try Again");
-        i--;
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  
+  for (let i = 0; i < numberOfWords; i++) {
+    let word = prompt("Please enter a word:");
+    if (word === null || word.trim() === "") {
+      alert("Please enter a valid word. Try again.");
+      i--;
     } else {
-        wordArray.push(wordEntered);
+      wordArray.push(word);
     }
-}
+  }
 
-for (let i = 0; i < numberOfWords; i++) {
-    let li = document.createElement("li");
-    li.id = "li" + i;
-    li.innerHTML = wordArray[i];
-      document.getElementById(ul01).appendChild(li);
-}
-
-        const firstSection = document.querySelector('#firstSection');
-        const secondSection = document.getElementById("secondSection");
-        let isClicked = true;
-
-        let transformWords = function () {
-            
-            if (isClicked) {
-                firstSection.style.display = 'none'; // Changed to 'none'
-                secondSection.style.display = 'block';
-                isClicked = false;
-            } else {
-                firstSection.style.display = 'block';
-                secondSection.style.display = 'none'; // Changed to 'none'
-                isClicked = true;
-            }
-        };
- document.getElementById("transform").addEventListener("click", transformWords);
-)};
+  
+  for (let i = 0; i < numberOfWords; i++) {
+    let li = document.getElementById("li" + i);
+    if (li) {
+      li.innerHTML = wordArray[i];
+      
+    }
+  }
+  const firstSection = document.getElementById("firstSection");
+  const secondSection = document.getElementById("secondSection");
+  const transformButton = document.getElementById("transformButton"); 
+    transformButton.addEventListener("click", function () {
+      if (currentSection === "firstSection") {
+        firstSection.classList.add("hidden");
+        secondSection.classList.remove("hidden");
+        currentSection = "secondSection";
+      } else {
+        firstSection.classList.remove("hidden");
+        secondSection.classList.add("hidden");
+        currentSection = "firstSection";
+      }
+    });
+});
