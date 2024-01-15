3841afe8c3a187f4b8324bafb332a17621434ccf
Marie <122072731+Mchuong10@users.noreply.github.com>
2024-01-14 21:48:18 -0800

    Update main.js

--- HW2/main.js
+++ HW2/main.js
@@ -1,31 +1,36 @@
 let numberOfWords = 3;
+
 let wordArray = [];
+
 let currentSection = 'firstSection';
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-  
-  for (let i = 0; i < numberOfWords; i++) {
-    let word = prompt("Please enter a word:");
-    if (word === null || word.trim() === "") {
-      alert("Please enter a valid word. Try again.");
-      i--;
-    } else {
-      wordArray.push(word);
+    for (let i = 0; i < numberOfWords; i++) {
+    
+        let wordEntered = prompt("Please enter a word:");
+        if (wordEntered === null || wordEntered.trim()=== ""){ 
+            alert("Please enter a valid word. Try Again");
+            i--;
+        } else { 
+        wordArray.push(wordEntered);
+        }
     }
-  }
 
-  
-  for (let i = 0; i < numberOfWords; i++) {
-    let li = document.getElementById("li" + i);
-    if (li) {
-      li.innerHTML = wordArray[i];
-      
+    for (let i = 0; i < numberOfWords; i++) {
+
+        let li = document.getElementById("li" + i)
+        
+        li.innerHTML = wordArray[i];
+
     }
-  }
-  const firstSection = document.getElementById("firstSection");
-  const secondSection = document.getElementById("secondSection");
-  const transformButton = document.getElementById("transformButton"); 
+
+    const firstSection = document.getElementById("firstSection");
+    const secondSection = document.getElementById("secondSection");
+    const transformButton = document.getElementById("transformButton"); 
+
+    transformButton.classList.remove("hidden");
+
     transformButton.addEventListener("click", function () {
       if (currentSection === "firstSection") {
         firstSection.classList.add("hidden");
@@ -36,5 +41,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
         secondSection.classList.add("hidden");
         currentSection = "firstSection";
       }
+
+      function letterSwitcher (word) {
+        return(word.charAt(word.length - 1) + word.substring(1, word.length - 1) + word.charAt(0));
+      }
+
+      let switchedArray = wordArray.map(letterSwitcher);
+
+      for (let i = 0; i < numberOfWords; i++) {
+
+        let li = document.getElementById("li" + (i + 3))
+        
+        li.innerHTML = switchedArray[i];
+
+    }
+
     });
 });
